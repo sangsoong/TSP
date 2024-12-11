52b10a7694f7d42e1fd35e9b68d3f9cf1e33c5e2
sangsoong <72246035+sangsoong@users.noreply.github.com>
2024-12-11 17:22:53 +0900

    jump animation, attack animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,19 +4,24 @@ using UnityEditor.PackageManager;
 
 public class Player : MonoBehaviour
 {
-	public float movePower = 1f;
-	public float jumpPower = 1f;
-
-    private float extraHeight = 0.1f;
-
 	Rigidbody2D rigid2D;
     BoxCollider2D box2D;
     SpriteRenderer spriteRD;
     Animator animator;
 
+	public float movePower = 1f;
+	public float jumpPower = 1f;
+
+    private float extraHeight = 0.1f;
     Vector3 movement;
+    private float currentAttackStiff;
+    public float attackStiffTime = 1f;
+
+    bool canMove = true;
 	bool canJump = true;
     bool isJumped = false;
+    bool isAttacked = false;
+    bool isAttacking = false;
     
     void Start()
     {
@@ -31,16 +36,41 @@ public class Player : MonoBehaviour
     void Update()
     {
 
+        // move
+        if (isAttacking)
+            canMove = false;
+        else
+            canMove = true;
+
+        // jump
         RaycastHit2D rayHit = Physics2D.Raycast(rigid2D.position, Vector3.down, box2D.bounds.extents.y + extraHeight, LayerMask.GetMask("Standable"));
 
-        if (rayHit.collider != null) {
+        if (rayHit.collider != null && isAttacking == false) {
             canJump = true;
+            animator.SetBool("isFlying", false);
         }
-        else
+        else {
             canJump = false;
+            animator.SetBool("isFlying", true);
+        }
 
-        if (canJump && Input.GetButtonDown("Jump"))
+        if (canJump && Input.GetKeyDown(KeyCode.W)) {
             isJumped = true;
+        }
+        
+        // Attack
+        if (currentAttackStiff <= 0) {
+            isAttacking = false;
+            if (Input.GetKeyDown(KeyCode.U)) {
+                isAttacked = true;
+                animator.SetBool("isAttacking", true);
+                currentAttackStiff = attackStiffTime;
+            }
+        }
+        else if (currentAttackStiff > 0) {
+            currentAttackStiff -= Time.deltaTime;
+            isAttacking = true;
+        }
 
     }
 
@@ -49,20 +79,24 @@ public class Player : MonoBehaviour
 
         Move();
         Jump();
+        Attack();
 
     }
 
     void Move()
     {
 
+        if (!canMove)
+            return;
+                
         Vector3 moveVelocity = Vector3.zero;
 
-        if (Input.GetAxisRaw("Horizontal") < 0) {
+        if (Input.GetKey(KeyCode.A)) {
             moveVelocity = Vector3.left;
             spriteRD.flipX = true;
             animator.SetBool("isMoving", true);
         }
-        else if (Input.GetAxisRaw("Horizontal") > 0) {
+        else if (Input.GetKey(KeyCode.D)) {
             moveVelocity = Vector3.right;
             spriteRD.flipX = false;
             animator.SetBool("isMoving", true);
@@ -86,6 +120,16 @@ public class Player : MonoBehaviour
         rigid2D.AddForce(jumpVelocity, ForceMode2D.Impulse);
 
         isJumped = false;
+
+    }
+
+    void Attack()
+    {
+
+        if (!isAttacked)
+            return;
+        
+        isAttacked = false;
 
     }
 }
